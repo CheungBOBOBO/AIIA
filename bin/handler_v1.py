8c27c1a286c5066c89ebc4a58706a6cb6f1cef57
cc <chongwangcc@gmal.com>
2018-09-25 17:44:10 +0800

    添加功能： 1. func2:过滤语料

--- bin/handler_v1.py
+++ bin/handler_v1.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 from bin.utils import *
 from bin.preprocess import *
 
-temp_path="../result/v1"
+temp_path="../result/v2"
 os.makedirs(temp_path,exist_ok=True)
 
 def func1():
@@ -13,7 +13,7 @@ def func1():
     :return:
     """
     # 1.获得所有字典路径
-    dicts_path=[os.path.join(root,file) for root,dir,files in os.walk("../data/my_dict/")
+    dicts_path=[os.path.join(root,file) for root,dir,files in os.walk("../data/my_dict")
                                         for file in files]
     # 2.加载字典名
     dict_result=[]
@@ -29,6 +29,22 @@ def func1():
 
     # 5.保存 字典
     save_list(dict_filer, "../etc/my.dict")
+
+def func2():
+    """
+    清理语料库，去掉乱码的句子，语料分词，统计词频
+    :return:
+    """
+    jieba.load_userdict("../etc/my.dict")
+    # 1.清理语料库
+    corpus_filter_lines()
+
+    # 2.语料分词
+    corpus_participle()
+
+    # 3.统计词频
+    word_count = corpus_word_count()
+    save_dict(word_count, os.path.join(temp_path,"words_count.csv"))
 
 def main():
     func1()
